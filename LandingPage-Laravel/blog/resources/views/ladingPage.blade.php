1d37957b3ff9deff975ea0ca43c80fb13a0f7cf8
Gabriel Souza <gabrielbsouzadev@gmail.com>
2020-08-15 16:25:15 -0300

    section 1 - Draft

---
+++ LandingPage-Laravel/blog/resources/views/ladingPage.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Landing Page - Rits</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <nav class="navbar navbar-light bg-white">
+                <img src="https://previews.dropbox.com/p/thumb/AA5kcehQJwE_kMHakkpztOlib9Ii6IG--_5edYd4fm6m8I5K6PtGhKeCTSkLAd8LIck-FmLKuYkJgnJ1wv_XztbmPwxb7meleNUJz2OF4XJ7P1UXImUWzKZPjDYOJEBBOQt21XdZHr-S4c9EyT773NdV-bj7FcBuwnFV4PRJz2JePvVxYX1xhH4rHFvwvuhBjzzLJPTXH0OdQ7-r89PBUSDTBrOZLep3HhQPekKY0X6j6_ZHMmLHFkrJ9NHZi0_uPQR0U1UK0nSIphGb2A87p0mMykGPbhRtOXduONTpS5BILv3NChMKdrVV0cL0-MDpNIXgqS-6iVVscb7BivUfQAaYyQ9ibohCC1XvGAncKSyZ-A/p.png?fv_content=true&size_mode=5" alt="">
+                <nav class="my-2 my-md-0 mr-md-5">
+                    <a class="p-2 text-dark" href="#">Prain</a>
+                    <a class="p-2 text-dark" href="#">Atendimento</a>
+                </nav>
+            </nav>
+        </header>
+
+        <div class="row mb-2">
+            <div class="col-md-6">
+                <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <div class="card mx-auto" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <img src="https://previews.dropbox.com/p/thumb/AA4oO6vlfZCbkQhOT8jGQ_Iu_CUuJ2hPMg1rPCd7ERnL1nJ-Nn6Q9r1Ebf6Kap8fl1wejhQTiE-exTW-ZaNd0b3GaFtHFBOHFhU_8jBy0yI7nsZPNbCukl8HpsOh6pZ0kZcXKADbHut_cJGNnJJ9EDTQScgdIPBuiFOdRjz69AXEqT9eX5F0CdbIUoxyAJD1I_R2Z78SVqAxQyjAbwPWhhTkN4uQCEtK7nNmOko4xpcDUui_mp0Eq8_bF_ThARF5gUbKfcfxg_TjLrwCosID46iurek326g-Pn8vzx4Cy5ZHSmANQJeEURNgMBQcroTbeWpjWC-2hmfXCQmQKG_No2yeJoTmAv0Mgn7saMHngLU3wRAHKgV5HwnRib3EhOA6Vuj2BaKYzT6m9667hk7JP2U8PAxL9YTuqPSlcfa1ZKF45qLojFWW8JZrgc5nqPpwN9-wJQdEcFmFbfjMTUabsGtXEj0PMD2M3TaA10WLKqVL1aZSCzeF0EDydAkQzI7oMd_qruf4SK1ft1ypQYLU3w5Yfb491_YWN7UNTfHwZ1DHUg/p.jpeg?fv_content=true&size_mode=5" alt="" height="250px">
+                        <strong class="d-inline-block mb-2 text-success">Colocar setinha verde</strong>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- LandingPage-Laravel/blog/routes/web.php
+++ LandingPage-Laravel/blog/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/lading-page', function () {
+    return view('ladingPage');
+});
