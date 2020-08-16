601c608faac985504960e677065b402b52a9d3ae
Gabriel Souza <gabrielbsouzadev@gmail.com>
2020-08-16 20:22:12 -0300

    walking for the finish

--- LandingPage-Laravel/blog/resources/views/ladingPage.blade.php
+++ LandingPage-Laravel/blog/resources/views/ladingPage.blade.php
@@ -7,13 +7,6 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-
-
-    <!-- icons -->
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-
-
     <title>Landing Page - Rits</title>
 </head>
 
@@ -47,7 +40,7 @@
     <div class="container">
         <header>
             <nav class="navbar navbar-light bg-white">
-                <img src="https://previews.dropbox.com/p/thumb/AA5kcehQJwE_kMHakkpztOlib9Ii6IG--_5edYd4fm6m8I5K6PtGhKeCTSkLAd8LIck-FmLKuYkJgnJ1wv_XztbmPwxb7meleNUJz2OF4XJ7P1UXImUWzKZPjDYOJEBBOQt21XdZHr-S4c9EyT773NdV-bj7FcBuwnFV4PRJz2JePvVxYX1xhH4rHFvwvuhBjzzLJPTXH0OdQ7-r89PBUSDTBrOZLep3HhQPekKY0X6j6_ZHMmLHFkrJ9NHZi0_uPQR0U1UK0nSIphGb2A87p0mMykGPbhRtOXduONTpS5BILv3NChMKdrVV0cL0-MDpNIXgqS-6iVVscb7BivUfQAaYyQ9ibohCC1XvGAncKSyZ-A/p.png?fv_content=true&size_mode=5" alt="">
+                <img src="/assets/images/section-1.png" alt="">
                 <nav class="my-2 my-md-0 mr-md-5">
                     <a class="p-2 text-dark" href="#">Prain</a>
                     <a class="p-2 text-dark" href="#">Atendimento</a>
@@ -55,61 +48,65 @@
             </nav>
         </header>
 
-        <div class="row mb-2">
-            <div class="col-md-6 sobre">
-                <div class="row no-gutters border-white overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <div class="border-white card mx-auto" style="width: 18rem;">
-                            <div class="card-body mt-3 ml-5">
-                                <h5 class="card-title">Consectetur adipiscing elit</h5>
-                                <p class="card-text"><b>Lorem ipsum:</b> dolor sit amet consectetur, adipisicing elit. Aspernatur, doloribus!</p>
-                                <button type="button" class="btn btn-danger">Quero me candidatar</button>
-                            </div>
+
+    </div>
+
+    <div class="row mb-2">
+        <div class="col-md-6 sobre">
+            <div class="row no-gutters border-white overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <div class="border-white card mx-auto" style="width: 18rem;">
+                        <div class="card-body mt-3 ml-5">
+                            <h5 class="card-title">Consectetur adipiscing elit</h5>
+                            <p class="card-text"><b>Lorem ipsum:</b> dolor sit amet consectetur, adipisicing elit. Aspernatur, doloribus!</p>
+                            <button type="button" class="btn btn-danger">Quero me candidatar</button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 sobre">
-                <img src="https://previews.dropbox.com/p/thumb/AA4oO6vlfZCbkQhOT8jGQ_Iu_CUuJ2hPMg1rPCd7ERnL1nJ-Nn6Q9r1Ebf6Kap8fl1wejhQTiE-exTW-ZaNd0b3GaFtHFBOHFhU_8jBy0yI7nsZPNbCukl8HpsOh6pZ0kZcXKADbHut_cJGNnJJ9EDTQScgdIPBuiFOdRjz69AXEqT9eX5F0CdbIUoxyAJD1I_R2Z78SVqAxQyjAbwPWhhTkN4uQCEtK7nNmOko4xpcDUui_mp0Eq8_bF_ThARF5gUbKfcfxg_TjLrwCosID46iurek326g-Pn8vzx4Cy5ZHSmANQJeEURNgMBQcroTbeWpjWC-2hmfXCQmQKG_No2yeJoTmAv0Mgn7saMHngLU3wRAHKgV5HwnRib3EhOA6Vuj2BaKYzT6m9667hk7JP2U8PAxL9YTuqPSlcfa1ZKF45qLojFWW8JZrgc5nqPpwN9-wJQdEcFmFbfjMTUabsGtXEj0PMD2M3TaA10WLKqVL1aZSCzeF0EDydAkQzI7oMd_qruf4SK1ft1ypQYLU3w5Yfb491_YWN7UNTfHwZ1DHUg/p.jpeg?fv_content=true&size_mode=5" alt="" height="350px" width="100%">
-            </div>
+        </div>
+        <div class="col-md-6 sobre">
+            <img src="/assets/images/section-2.jpg" alt="" height="400px" width="100%">
         </div>
     </div>
 
     <div class="jumbotron bg-success">
-        <div class="container">
-            <div class="container row retire-margin">
-                <div class="col-md-8 bg-white ml-5 pl-0 pr-0  sobsec2">
-                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-sanger">Design</strong>
-                            <h3 class="mb-0">Post title</h3>
-                            <div class="mb-1 text-muted">Nov 11</div>
-                            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="stretched-link">Continue reading</a>
-                        </div>
-                        <div class="col-auto d-none d-lg-block opaimg">
-                            <img src="https://previews.dropbox.com/p/thumb/AA4C5SH9PjteFGKdOIk6lqys4RD4i_4C-CumOUo5w-F9c6cdYCsa39s9BpSpih0iackYHnHWrm_QzF__1-DD6SO55zxt6yZZOEVbvMNnoqxLaY6A7BLnIs2ZTi51c-qw9mFhumOm87W_XwCF6cF5lMZmIoAI38XFvsnd6eLUMXmksR_OazQ1Vk1yt3EQE83vCpnnHDHNom5jyvwS9q9xkSy0RAMDgPlkFwDmKgaSxgc7kr3afagDgjR_WEVMjf9gmvzrTiepn-EZkbdsdtAlZChGUsSR20sFKL12b9LzojEuyxQxS-Qmvym9ZdOO8R4fxU27mN8zmvuR2frS156mwj4Avn1DVgIkughvz3iIx6uKXhYwWSPU4oJCFq7kXEqH0HRO3EHK0YtihpxVXpMXsgibHkhOc7dKR8At29zSXTNi_UmqYcC3As6kT2FN0PS2QJDYzX5m0wngxCVbHU-Fkkxp274ek1bT8KWwwVQbKRCdIQ/p.jpeg?size=1280x960&size_mode=3" alt="" height="225px" width="300px">
-                        </div>
-                    </div>
-                </div>
+        <div class="container" style="margin-bottom: -165px;">
+
+            <style>
+                #myDIV {
+
+                    width: 600px;
+                    height: 300px;
+                    /* border: 1px solid black; */
+                    margin: -270px 350px auto auto;
+                }
+
+                #DIV2 {
+                    position: relative;
+                    top: 130px;
+                    left: 30px;
+                    width: 300px;
+                    height: 400px;
+                    /* background-color: coral;
+                    border: 1px solid black;
+                    margin: auto;
+                    background-image: url('/assets/images/section-3.jpg');
+                    background-repeat: no-repeat; */
+                    filter: brightness(30%); 
+                }
+            </style>
+
+            <div id="DIV2">
+                <img src="/assets/images/section-3.jpg" alt="" height="200px" width="100%" style="margin: auto auto auto 180%;">
+            </div>
+
+            <div id="myDIV" class="bg-white text-black border-white">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, id. Repellendus quidem, pariatur esse optio earum deserunt quam labore nesciunt laboriosam suscipit culpa aliquam fugit, quaerat cum distinctio veniam autem!</p>
             </div>
         </div>
     </div>
-
-    <!-- <div class="col p-4 d-flex flex-column position-static">
-
-        <h3 class="mb-0">Post title</h3>
-        <div class="mb-1 text-muted">Nov 11</div>
-        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-
     </div>
-    <div>
-        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail">
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-        </svg>
-    </div>
-    <img src="https://previews.dropbox.com/p/thumb/AA5-PSrrnUhva1AgaHyq0aOpeXYmKwVpuM-VxBp7vqf9F52CGN2-6T-xRswx5z7a4xFD7vplnD_ipmjSXqdshvSeoDdvMS-a6h9E08WczgCKDvWIR6U0W6QdCOeAIj7lndVx9WqTQ26jgMmC5hE-3nSZyS6RZ2G0nbfTNhlMschalEbzzO4hTuc0mAKAb5jT2DJ16kC5vk4uKjA9aYWhK7bsDsawp3QosxHd0yNOY5st14dmTq7-zo7EtcohFbRf__eJrblpPUOxPAda0xm4irMI6IA8NMvbuX5Fh0RilIuz7s6PAp-0jj-OFOCpMSJUc3g_ZEkLREZ-BPGvfkwIGK-EP0n-9nMCUVd6eXR2z-HENsxPeikPBdlPJC5Lgonbulu1fLrprRreH5Z9_51DbX3QBUGqGu3MXL3-w2icH9i0ZxSt_86MmL9rXLvJClBcUBBeD6tNcs6SPmox8eYqgwNgu_QV9SkwqdkNyULmMo25qM1JBiUNOmlKwC7lgqotQB0bUESH3tk9JuePhlm5a5rSWFLOEbbR_-PndwJprtT_Qw/p.jpeg?size=1280x960&size_mode=3" alt="" height="50%" width="75%" style="margin-top: -100px"> -->
 
     <div class="container">
         <div class="row mb-2">
@@ -129,7 +126,7 @@
             <div class="col-md-6">
                 <div class="mx-auto">
                     <div class="card-body mt-3 ml-5">
-                        <img src="https://previews.dropbox.com/p/thumb/AA6sey1hL-_OvdYbTlXbEeMhrppQJfCKdIO0ClbSvQIrlA1k7CfEu1esfxQadIrOw_cZmXPBMLFIAYvIZ6kQl7jej8ea9nchx700YxmdvDjxIL1woTfS5hwWBJGFgL1rvZUgDAWYhOClu6Zb9AwsC0K4hfgjrZnUiqvy0lT17yuGpXr6hbfZDQn1fcumc-EirRbQDkMeVs0xjkwNPlMFDtVfHWrPJBE1JkPoM3ZGzAH4mpZOLlwduzWjXeOtJumI0OAgGp5MoMSkVb8MtthmekVj5q0QUmbF5uu6MpqswT-y8LM3ex24fjfYdg8uXFnSqInpTQw6uzLSYV9LijxDBr0KpSgR5b48Dtek12a1eHK00w/p.png?fv_content=true&size_mode=5" alt="" height="350px" width="50%">
+                        <img src="/assets/images/section-4.png" alt="" height="350px" width="50%">
                     </div>
                 </div>
             </div>
@@ -139,7 +136,7 @@
 
     <div class="row mb-2">
         <div class="col-md-6 bg-success">
-            <img src="https://previews.dropbox.com/p/thumb/AA67Hrj3Q_TLOGdAzkAQsw9wAdVMQmFSUpXMHG-HeLAUQpfRiHxdIyRCm81Zkj1YgfTxmFcWoQW_8nAoR5y0K9631z2R7qu-3aWodPk1JLxwfJBnFNkU3BftYYJZY0GVQcrwkyYXKEwvM8RcT4YmBPBh94Ly396nfRvKEyneV0zZfH_c5SRfXwsZca5Voqan-L2q7r0lkBa-azmWdZHLilafH_wwdnt-hsgQ4_ugqP7lfY3PtK5G230c_U1MFHGZZRdI_dxSOn5wUW5MhGgaaoDfB40E_P5mYAL88OTau7sLSppnEK6B3aIu0-_B-UX0bVNV1hvHWJV6WdHDce6X2GtJICfdcGXil1LrUDNzsddZWr28-dQTb6TmIIVBzZfNxBy2Ow6T71tx0qVWVWSFzH4QKOcOjeuE_vW57mESGabjif4i9YX1IP-goAim1Y-dsGsoBsDGPznFS74CXJHsNjX6IA9zEHIM3ev1e50HtmUvag/p.jpeg?size=1280x960&size_mode=3" alt="" height="50%" width="75%" style="margin-top: -135px;margin-left: 170px;">
+            <img src="/assets/images/section-5.jpg" alt="" height="50%" width="75%" style="margin-top: -135px;margin-left: 170px;">
             <div class="row no-gutters border-sucess overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style="margin-left: 50px">
                 <div class="col p-4 d-flex flex-column position-static">
                     <div class="border-sucess card mx-auto bg-success" style="width: 18rem;">
@@ -229,7 +226,7 @@
                         <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
                     </svg>
                 </div>
-                <img src="https://previews.dropbox.com/p/thumb/AA5-PSrrnUhva1AgaHyq0aOpeXYmKwVpuM-VxBp7vqf9F52CGN2-6T-xRswx5z7a4xFD7vplnD_ipmjSXqdshvSeoDdvMS-a6h9E08WczgCKDvWIR6U0W6QdCOeAIj7lndVx9WqTQ26jgMmC5hE-3nSZyS6RZ2G0nbfTNhlMschalEbzzO4hTuc0mAKAb5jT2DJ16kC5vk4uKjA9aYWhK7bsDsawp3QosxHd0yNOY5st14dmTq7-zo7EtcohFbRf__eJrblpPUOxPAda0xm4irMI6IA8NMvbuX5Fh0RilIuz7s6PAp-0jj-OFOCpMSJUc3g_ZEkLREZ-BPGvfkwIGK-EP0n-9nMCUVd6eXR2z-HENsxPeikPBdlPJC5Lgonbulu1fLrprRreH5Z9_51DbX3QBUGqGu3MXL3-w2icH9i0ZxSt_86MmL9rXLvJClBcUBBeD6tNcs6SPmox8eYqgwNgu_QV9SkwqdkNyULmMo25qM1JBiUNOmlKwC7lgqotQB0bUESH3tk9JuePhlm5a5rSWFLOEbbR_-PndwJprtT_Qw/p.jpeg?size=1280x960&size_mode=3" alt="" height="50%" width="75%" style="margin-top: -100px">
+                <img src="/assets/images/section-6.jpg" alt="" height="50%" width="75%" style="margin-top: -100px">
                 <button type="button" class="btn btn-danger" style="margin-left: 25%;margin-top: -20px;">Quero me candidatar</button>
             </div>
 
@@ -252,7 +249,7 @@
                         <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
                     </svg>
                 </div>
-                <img src="https://previews.dropbox.com/p/thumb/AA4_MLaG5cIqunJdbXxVP4plbdGsaH3Lev35LSU73eztQ806a0H5Av352EW9XP0vDlfWnCMjdJzi2B775Tc6olc3jQVpCh_A70grLkdcHjLoLISWtxrBJFvLk7C3wntF5ap4GBF8h21rYfWy-xWMUwwG6XPVBw1DjFFRvXqpPlul97DaQS5mAuIXd8IrSFcr4cMKZy4aE8GFKUhdr2CNfDs5SFUjy_tG_vSyvfLAXSra6I3yUsR0UdHLgt5Kx1qMngUDgU6e4U73UdPXGRzDX3ZA4R50ZDORnwZiUjFQdmgwTuvB-PJ2n6M2aqSR4A8IaLUFR6Bs_pyNu4LRnZLHiKtic2WxCQ_edFk2qvSlovkM4Dl9vWRMtTklBYIoF7n2siHwqc7m48vUxOXKicar4oxEjbRGFNcaR-4gwqQqfxt7DbZbyyH6101HCdNl9hXqPDnI5GGych1Sia7_Ey61D-yAofEw-TELCPvUKR6Xqt33Y-tzpe_L8SknSpmQxSH6iC7DBJc_nXFnY4Dgilvkbob7bVz9C5KPHBD4u-DepHP9IiEkng0I3YYRyUuTri1JMFuv6Rk_TOtuezJowpK5btSv/p.jpeg?fv_content=true&size_mode=5" alt="" height="50%" width="75%" style="margin-left: 155px">
+                <img src="/assets/images/section-7.jpg" alt="" height="50%" width="75%" style="margin-left: 155px">
                 <button type="button" class="btn btn-danger" style="margin-left: 50%;margin-top: -20px;">Quero me candidatar</button>
             </div>
 
@@ -293,7 +290,7 @@
         <div class="col-md-6 bg-light">
             <div class="mx-auto">
                 <div class="card-body mt-3 ml-5">
-                    <img src="https://previews.dropbox.com/p/thumb/AA58fEqImZZSLTrX6dC2mYgT761fcbpNvXEoPkrJxq7xzRgqYqsozWEu9MrOWoATNkPyuZDMOzlLgdVcCe1wf463ixy6km5dE9jVDewmuOM6-WpdPS1Jt8KKSLEeuFzdMk7uhGUhPstUB6KZ3GQrzkgtp4x8-Kreomjz4339XgFmCdCKo0x5pbVApETHFvRlcWHU_349rWW-GPr9C5apEuK9ZyVk9AV-0UCJrDYNmB-oXWMOK6miTTZ9FhdaN1hh4dre_ji5nQZyuuLlOyUvpxIAXp0FYHD0-eZHzhb4hODVRwyasrc-JCEwxPeCtPAdVD8my-GGYSCY5O9LMeTu67Wgjj6TyanIj1U30KCdPkhurQ/p.png?fv_content=true&amp;size_mode=5" alt="" height="100%" width="50%" style="margin-top: -90px;">
+                    <img src="/assets/images/section-8.png" alt="" height="100%" width="50%" style="margin-top: -90px;">
                 </div>
             </div>
         </div>
@@ -307,7 +304,7 @@
                             <h5 class="card-title">Light card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-                        <img class="Card image cap" style="height: 100px;width: 25%;display: block;align-items: center;text-align: center;margin: auto;" src="https://previews.dropbox.com/p/thumb/AA6qxRlbRbVEsiDy7uabnx9qE2NYHag4aJhLSQK8oZ1DSE8KMbIUl2_SGCItFUeNtIXtUA6h33h-pnmqSsYa4j0L5qkPyyjvug4kH6jqHtpxf3SEuz6hsYk9jtjB3gCda0tCQj3KEZV0rGR3N7pJk6Vk651XYl2ak8I8jGT2IpbNSdyZpuaiVNMHqjXyTgV-pw6xOA3zidKB8xk343vqG-rN5mkGbBkboAKNeFoD1W8-m5k_OWrcb-s1ciWETOCQrc1MJoW40jxbhEH9yAzUkpN8ftYayk-LIh-MvoXn8s82WyJh8UQjSpnD-TOAywFLSMqbCiupSfrhNOy8TmWqMMrZsmC2oI2uzz9ulBd1dbnhDw/p.png?fv_content=true&size_mode=5" alt="Card image cap">
+                        <img class="Card image cap" style="height: 100px;width: 25%;display: block;align-items: center;text-align: center;margin: auto;" src="/assets/images/card-1-img-section-9.png" alt="Card image cap">
                     </div>
 
                     <div class="card border-light" style="max-width: 18rem;">
@@ -316,7 +313,7 @@
                             <h5 class="card-title">Light card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-                        <img class="Card image cap" style="height: 100px;width: 25%;display: block;align-items: center;text-align: center;margin: auto;" src="https://previews.dropbox.com/p/thumb/AA6qxRlbRbVEsiDy7uabnx9qE2NYHag4aJhLSQK8oZ1DSE8KMbIUl2_SGCItFUeNtIXtUA6h33h-pnmqSsYa4j0L5qkPyyjvug4kH6jqHtpxf3SEuz6hsYk9jtjB3gCda0tCQj3KEZV0rGR3N7pJk6Vk651XYl2ak8I8jGT2IpbNSdyZpuaiVNMHqjXyTgV-pw6xOA3zidKB8xk343vqG-rN5mkGbBkboAKNeFoD1W8-m5k_OWrcb-s1ciWETOCQrc1MJoW40jxbhEH9yAzUkpN8ftYayk-LIh-MvoXn8s82WyJh8UQjSpnD-TOAywFLSMqbCiupSfrhNOy8TmWqMMrZsmC2oI2uzz9ulBd1dbnhDw/p.png?fv_content=true&size_mode=5" alt="Card image cap">
+                        <img class="Card image cap" style="height: 10%;width: 25%;display: block;align-items: center;text-align: center;margin: auto auto 10% auto;" src="/assets/images/card-2-img-section-9.png" alt="Card image cap">
                     </div>
                     <div class="card border-light" style="max-width: 18rem;">
                         <div class="card-header text-center">Header</div>
@@ -324,7 +321,7 @@
                             <h5 class="card-title">Light card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-                        <img class="Card image cap" style="height: 100px;width: 25%;display: block;align-items: center;text-align: center;margin: auto;" src="https://previews.dropbox.com/p/thumb/AA6qxRlbRbVEsiDy7uabnx9qE2NYHag4aJhLSQK8oZ1DSE8KMbIUl2_SGCItFUeNtIXtUA6h33h-pnmqSsYa4j0L5qkPyyjvug4kH6jqHtpxf3SEuz6hsYk9jtjB3gCda0tCQj3KEZV0rGR3N7pJk6Vk651XYl2ak8I8jGT2IpbNSdyZpuaiVNMHqjXyTgV-pw6xOA3zidKB8xk343vqG-rN5mkGbBkboAKNeFoD1W8-m5k_OWrcb-s1ciWETOCQrc1MJoW40jxbhEH9yAzUkpN8ftYayk-LIh-MvoXn8s82WyJh8UQjSpnD-TOAywFLSMqbCiupSfrhNOy8TmWqMMrZsmC2oI2uzz9ulBd1dbnhDw/p.png?fv_content=true&size_mode=5" alt="Card image cap">
+                        <img class="Card image cap" style="height: 10%;width: 25%;display: block;align-items: center;text-align: center;margin: auto auto 10% auto;" src="/assets/images/card-3-img-section-9.png" alt="Card image cap">
                     </div>
                 </div>
             </div>
