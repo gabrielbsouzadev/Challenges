c7f939728f95ac107bac8e8a551024a56ecae8b7
Gabriel Souza <gabrielbsouzadev@gmail.com>
2020-08-16 16:09:37 -0300

    draft layout

--- LandingPage-Laravel/blog/resources/views/ladingPage.blade.php
+++ LandingPage-Laravel/blog/resources/views/ladingPage.blade.php
@@ -398,13 +398,27 @@
         </div>
     </div>
 
-    <div class="jumbotron p-4 p-md-5 text-white rounded bg-light">
-        <div class="col-md-6 px-0">
-            <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-            <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-            <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+    <footer>
+
+        <div class="jumbotron p-4 p-md-5 rounded bg-light">
+            <div class="container">
+                <ul class="list-group list-group-horizontal" style="justify-content: center; margin-top: -100px;">
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Morbi leo risus</li>
+                </ul>
+
+                <div class="container mx-auto">
+                    <p class="float-right"><a href="#">Back to top</a></p>
+                    <p>© 2017-2020 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+                </div>
+            </div>
+
+
+
         </div>
-    </div>
+    </footer>
+
 
 </body>
 
